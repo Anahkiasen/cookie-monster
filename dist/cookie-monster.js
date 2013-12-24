a969944ee94cc1b2a4b764999e73a4e89fe1551a
Maxime Fabre <ehtnam6@gmail.com>
2013-12-24 03:00:10 +0100

    Rebuild files

--- dist/cookie-monster.js
+++ dist/cookie-monster.js
@@ -53,10 +53,11 @@ var CookieMonster = {
 	// Selectors
 	////////////////////////////////////////////////////////////////////
 
-	$overlay       : $('#cookie_monster_overlay'),
+	$goldenCookie  : $('#goldenCookie'),
 	$goldenOverlay : $('#cookie_monster_golden_overlay'),
-	$monsterBar    : $("#cookie_monster_bar"),
-	$goldenCookie  : $("#goldenCookie"),
+	$monsterBar    : $('#cookie_monster_bar'),
+	$overlay       : $('#cookie_monster_overlay'),
+	$timerBars     : $('#cookie_monster_timer_bars_div'),
 
 	// Colors
 	////////////////////////////////////////////////////////////////////
@@ -180,121 +181,6 @@ CookieMonster.Emphasizers.flashScreen = function() {
 
 	CookieMonster.$overlay.fadeIn(100);
 	CookieMonster.$overlay.fadeOut(500);
-};
-
-//////////////////////////////////////////////////////////////////////
-/////////////////////////////// BUFF BARS ////////////////////////////
-//////////////////////////////////////////////////////////////////////
-
-CookieMonster.manageBuffs = function() {
-	var buff       = "";
-	var color      = "";
-	var multiplier = 0;
-	var i          = 13 + 13 * Game.Has("Get lucky");
-	var s          = new Array(Game.goldenCookie.time, Game.goldenCookie.minTime, Game.goldenCookie.maxTime);
-	var o          = parseInt($("#cookie_monster_timer_bars_div").css("width"));
-
-	switch (Game.frenzyPower) {
-		case 7:
-			multiplier = 77 + 77 * Game.Has("Get lucky");
-			buff       = "Frenzy";
-			color      = this.colors.yellow;
-			break;
-
-		case 666:
-			multiplier = 6 + 6 * Game.Has("Get lucky");
-			buff       = "Blood Frenzy";
-			color      = this.colors.green;
-			break;
-
-		// This is wrong but I'm not sure what to change it to
-		case 0.5:
-			multiplier = 66 + 66 * Game.Has("Get lucky");
-			buff       = "Clot";
-			color      = this.colors.red;
-			break;
-	}
-
-	if (Game.frenzy > 0 && this.getSetting('BuffBars', true)) {
-		if ($("#cookie_monster_timer_" + color).length !== 1) {
-			$("#cookie_monster_timer_bars_div").append(
-				'<div id="cookie_monster_timer_' + color + '" style="padding:4px 0px 5px 0px;">'+
-				'<table cellpadding=0 cellspacing=0 style="font-style:inherit; color:inherit;  width:100%;">'+
-					'<tr>' +
-						'<td style="width:130px; text-align:right;">' + buff + "<td>" +
-						'<td>'+
-							'<div id="cmt_' + color + '" style="position:relative; background:#' + color + "; height:10px; width:" + Game.frenzy / s[2] * 100 + '%; margin-left:4px; border:1px solid black;">'+
-								'<div id="cmt_time_' + color + '" style="text-align:left; position:absolute; right:-50px; top:-5px; width:45px;">' +
-								multiplier +
-								'</div>'+
-							'</div>'+
-						'</td>'+
-						'<td style="width:55px;"></td>'+
-					'</tr>' +
-				'</table>'+
-			'</div>');
-
-		} else {
-			$("#cmt_" + color).css("width", Game.frenzy / s[2] * 100 + "%");
-			$("#cmt_time_" + color).text(Math.round(Game.frenzy / Game.fps));
-		}
-
-		$("#cookie_monster_timer_" + color).fadeIn(250);
-		var buffColors = ['FFFF00', '00FF00', 'FF0000'];
-		for (var thisColor in buffColors) {
-			this.fadeOutBar(buffColors[thisColor], color);
-		}
-	} else {
-		this.fadeOutBar(color);
-	}
-
-	var countdown = Math.round((s[2] - s[0]) / Game.fps);
-
-	if (Game.clickFrenzy > 0 && this.getBooleanSetting('BuffBars')) {
-		if ($("#cookie_monster_timer_"+this.colors.blue).length !== 1) {
-			$("#cookie_monster_timer_bars_div").append('<div id="cookie_monster_timer_4BB8F0" style="padding:4px 0px 5px 0px;"><table cellpadding=0 cellspacing=0 style="font-style:inherit; color:inherit;  width:100%;"><tr>' + '<td style="width:130px; text-align:right;">Click Frenzy<td>' + '<td><div id="cmt_4BB8F0" style="position:relative; background:#4BB8F0; height:10px; width:' + Game.clickFrenzy / s[2] * 100 + '%; margin-left:4px; border:1px solid black;"><div id="cmt_time_4BB8F0" style="text-align:left; position:absolute; right:-50px; top:-5px; width:45px;">' + i + "</div></div></td>" + '<td style="width:55px;"></td>' + "</table></div>");
-		} else {
-			$("#cmt_"+this.colors.blue).css("width", Game.clickFrenzy / s[2] * 100 + "%");
-			$("#cmt_time_"+this.colors.blue).text(Math.round(Game.clickFrenzy / Game.fps));
-		}
-		$("#cookie_monster_timer_"+this.colors.blue).fadeIn(250);
-	} else {
-		this.fadeOutBar(this.colors.blue);
-	}
-
-	if (s[0] > 0 && CookieMonster.$goldenCookie.css("display") === "none" && this.getBooleanSetting('CookieCD')) {
-		if ($("#cookie_monster_timer_"+this.colors.purple).length !== 1) {
-			$("#cookie_monster_timer_bars_div").append('<div id="cookie_monster_timer_FF00FF" style="padding:4px 0px 5px 0px;"><table cellpadding=0 cellspacing=0 style="font-style:inherit; color:inherit;  width:100%;"><tr>' + '<td style="width:130px; text-align:right;">Next Cookie<td>' + '<td><div id="cmt_FF00FF" style="position:relative; background:#aaaaaa; height:10px; width:100%; margin-left:4px; border:1px solid black;"><div id="cmt2_FF00FF" style="position:relative; background:#FF00FF; height:10px; width:100%; margin-left:0px; max-width:' + (o - 189) * 0.67 + 'px; float:right;"></div><div id="cmt_time_FF00FF" style="text-align:left; position:absolute; right:-50px; top:-5px; width:45px;">' + countdown + "</div></div></td>" + '<td style="width:55px;"></td>' + "</table></div>");
-		} else {
-			$("#cmt2_"+this.colors.purple).css("max-width", (o - 189) * 0.67 + "px");
-			$("#cmt_"+this.colors.purple).css("width", (s[2] - s[0]) / s[2] * 100 + "%");
-			$("#cmt_time_"+this.colors.purple).text(countdown);
-		}
-		$("#cookie_monster_timer_"+this.colors.purple).fadeIn(250);
-	} else {
-		this.fadeOutBar(this.colors.purple);
-	}
-
-	if (countdown > 0 && CookieMonster.$goldenCookie.is(':hidden')) {
-		this.goldenCookieAvailable = this.getBooleanSetting('CookieCD') ? "(" + countdown + ") " : '';
-	}
-
-	$("#versionNumber").css("bottom", $("#cookie_monster_timer_bars_div").css("height"));
-};
-
-/**
- * Fade out a bar of a certain color
- *
- * @param {string} color
- *
- * @return {void}
- */
-CookieMonster.fadeOutBar = function(color, match) {
-	var $bar = $("#cookie_monster_timer_" + color);
-
-	if ($bar.length === 1 && $bar.css("opacity") === "1" && color !== match) {
-		$bar.stop(true, true).fadeOut(250);
-	}
 };
 /**
  * Get the number of Heavenly Chips from a number of cookies (all time)
@@ -1287,6 +1173,183 @@ CookieMonster.updateTable = function() {
 	});
 };
 /**
+ * Updates the various buff bars
+ *
+ * @return {void}
+ */
+CookieMonster.manageBuffs = function() {
+	this.manageFrenzyBars();
+	this.manageClickingFrenzy();
+	this.manageNextCookie();
+
+	// Offset version number
+	$('#versionNumber').css('bottom', this.$timerBars.css('height'));
+};
+
+//////////////////////////////////////////////////////////////////////
+////////////////////////////////// BARS //////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+/**
+ * Manage regular frenzies bars
+ *
+ * @return {void}
+ */
+CookieMonster.manageFrenzyBars = function() {
+	var frenzyName = '';
+	var color      = '';
+	var multiplier = 0;
+
+	// Detect what kind of frenzy we're in
+	switch (Game.frenzyPower) {
+		case 7:
+			multiplier = 77 + 77 * Game.Has("Get lucky");
+			frenzyName = "Frenzy";
+			color      = this.colors.yellow;
+			break;
+
+		case 666:
+			multiplier = 6 + 6 * Game.Has("Get lucky");
+			frenzyName = "Blood Frenzy";
+			color      = this.colors.green;
+			break;
+
+		case 0.5:
+			multiplier = 66 + 66 * Game.Has("Get lucky");
+			frenzyName = "Clot";
+			color      = this.colors.red;
+			break;
+	}
+
+	// Remove bars if the frenzy has ended or we disabled them
+	if (Game.frenzy <= 0 || !this.getBooleanSetting('BuffBars')) {
+		return this.fadeOutBar(color);
+	}
+
+	this.updateBar(frenzyName, color, Game.frenzy);
+
+	// No idea what that does
+	var buffColors = [this.colors.yellow, this.colors.green, this.colors.red];
+	for (var thisColor in buffColors) {
+		this.fadeOutBar(buffColors[thisColor], color);
+	}
+};
+
+/**
+ * Manage clicking frenzies bars
+ *
+ * @return {void}
+ */
+CookieMonster.manageClickingFrenzy = function() {
+	if (Game.clickFrenzy <= 0 || !this.getBooleanSetting('BuffBars')) {
+		return this.fadeOutBar(this.colors.blue);
+	}
+
+	this.updateBar('Click frenzy', this.colors.blue, Game.clickFrenzy);
+};
+
+/**
+ * Manage the "Next cookie" bar
+ *
+ * @return {void}
+ */
+CookieMonster.manageNextCookie = function() {
+	var barsWidth = parseInt(this.$timerBars.css('width'));
+	var timers    = [Game.goldenCookie.time, Game.goldenCookie.minTime, Game.goldenCookie.maxTime];
+	var width     = timers[2] - timers[0];
+	var countdown = Math.round(width / Game.fps);
+
+	// Update title
+	if (countdown > 0 && CookieMonster.$goldenCookie.is(':hidden')) {
+		this.goldenCookieAvailable = this.getBooleanSetting('CookieCD') ? "(" + countdown + ") " : '';
+	}
+
+	// Cancel if disabled
+	if (timers[0] <= 0 || CookieMonster.$goldenCookie.is(':visible') || !this.getBooleanSetting('CookieCD')) {
+		return this.fadeOutBar(this.colors.purple);
+	}
+
+	this.updateBar('Next Cookie', this.colors.purple, width, width / timers[2] * 100);
+	$('#cmt2_'+this.colors.purple).css('max-width', (barsWidth - 189) * 0.67 + "px");
+};
+
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////// HELPERS /////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+/**
+ * Update a buff bar's informations
+ *
+ * @param {String}  name
+ * @param {String}  color
+ * @param {Integer} timer
+ * @param {Integer} width
+ *
+ * @return {void}
+ */
+CookieMonster.updateBar = function (name, color, timer, width) {
+	var $bar = $('#cookie_monster_timer_'+color);
+	if ($bar.length !== 1) {
+		this.createBar(name, color);
+	}
+
+	// Define text count and CSS width
+	var count = timer / Game.fps;
+	if (typeof width === 'undefined') {
+		width = timer / Game.goldenCookie.maxTime * 100;
+	}
+
+	$('#cmt_'+color).css('width', width);
+	$('#cmt_time_'+color).text(Math.round(count));
+	$bar.fadeIn(250);
+};
+
+/**
+ * Append a timer bar
+ *
+ * @param {String}  color
+ * @param {Integer} count
+ *
+ * @return {void}
+ */
+CookieMonster.createBar = function (name, color) {
+	var secondBar = '';
+	if (name === 'Next Cookie') {
+		secondBar = '<div id="cmt2_'+this.colors.purple+'" style="position:relative; background:#' +this.colors.purple+'; height:10px; width:100%; margin-left:0px; max-width:0px; float:right;"></div>';
+	}
+
+	this.$timerBars.append(
+		'<div id="cookie_monster_timer_' + color + '" style="padding:4px 0px 5px 0px;">'+
+		'<table cellpadding=0 cellspacing=0 style="font-style:inherit; color:inherit; width:100%;">'+
+			'<tr>' +
+				'<td style="width:130px; text-align:right;">' + name + "<td>" +
+				'<td>'+
+					'<div id="cmt_' + color + '" style="position:relative; background:#' + color + '; height:10px; width:100%; margin-left:4px; border:1px solid black;">'+
+						secondBar +
+						'<div id="cmt_time_' + color + '" style="text-align:left; position:absolute; right:-50px; top:-5px; width:45px;">0</div>'+
+					'</div>'+
+				'</td>'+
+				'<td style="width:55px;"></td>'+
+			'</tr>' +
+		'</table>'+
+	'</div>');
+};
+
+/**
+ * Fade out a bar of a certain color
+ *
+ * @param {string} color
+ *
+ * @return {void}
+ */
+CookieMonster.fadeOutBar = function(color, match) {
+	var $bar = $("#cookie_monster_timer_" + color);
+
+	if ($bar.length === 1 && $bar.css("opacity") === "1" && color !== match) {
+		$bar.stop(true, true).fadeOut(250);
+	}
+};
+/**
  * Update the stylings of the upgrades to the selected option
  *
  * @return {void}
@@ -2126,10 +2189,10 @@ CookieMonster.start = function() {
 	$('link[href="favicon.ico"]').attr('id', 'cm_favicon');
 
 	// Refrehs selector
-	this.$overlay       = $('#cookie_monster_overlay');
 	this.$goldenOverlay = $('#cookie_monster_golden_overlay');
-	this.$monsterBar    = $("#cookie_monster_bar");
-	this.$goldenCookie  = $("#goldenCookie");
+	this.$monsterBar    = $('#cookie_monster_bar');
+	this.$overlay       = $('#cookie_monster_overlay');
+	this.$timerBars     = $('#cookie_monster_timer_bars_div');
 
 	this.makeTable();
 	this.saveTooltips();
