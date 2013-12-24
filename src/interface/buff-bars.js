7a4932d5ea2a65bc784fa583318b8f7714e8ba52
Maxime Fabre <ehtnam6@gmail.com>
2013-12-24 02:59:59 +0100

    Move handling of buff bars into a separate file

---
+++ src/interface/buff-bars.js
@@ -0,0 +1,177 @@
+/**
+ * Updates the various buff bars
+ *
+ * @return {void}
+ */
+CookieMonster.manageBuffs = function() {
+	this.manageFrenzyBars();
+	this.manageClickingFrenzy();
+	this.manageNextCookie();
+
+	// Offset version number
+	$('#versionNumber').css('bottom', this.$timerBars.css('height'));
+};
+
+//////////////////////////////////////////////////////////////////////
+////////////////////////////////// BARS //////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+/**
+ * Manage regular frenzies bars
+ *
+ * @return {void}
+ */
+CookieMonster.manageFrenzyBars = function() {
+	var frenzyName = '';
+	var color      = '';
+	var multiplier = 0;
+
+	// Detect what kind of frenzy we're in
+	switch (Game.frenzyPower) {
+		case 7:
+			multiplier = 77 + 77 * Game.Has("Get lucky");
+			frenzyName = "Frenzy";
+			color      = this.colors.yellow;
+			break;
+
+		case 666:
+			multiplier = 6 + 6 * Game.Has("Get lucky");
+			frenzyName = "Blood Frenzy";
+			color      = this.colors.green;
+			break;
+
+		case 0.5:
+			multiplier = 66 + 66 * Game.Has("Get lucky");
+			frenzyName = "Clot";
+			color      = this.colors.red;
+			break;
+	}
+
+	// Remove bars if the frenzy has ended or we disabled them
+	if (Game.frenzy <= 0 || !this.getBooleanSetting('BuffBars')) {
+		return this.fadeOutBar(color);
+	}
+
+	this.updateBar(frenzyName, color, Game.frenzy);
+
+	// No idea what that does
+	var buffColors = [this.colors.yellow, this.colors.green, this.colors.red];
+	for (var thisColor in buffColors) {
+		this.fadeOutBar(buffColors[thisColor], color);
+	}
+};
+
+/**
+ * Manage clicking frenzies bars
+ *
+ * @return {void}
+ */
+CookieMonster.manageClickingFrenzy = function() {
+	if (Game.clickFrenzy <= 0 || !this.getBooleanSetting('BuffBars')) {
+		return this.fadeOutBar(this.colors.blue);
+	}
+
+	this.updateBar('Click frenzy', this.colors.blue, Game.clickFrenzy);
+};
+
+/**
+ * Manage the "Next cookie" bar
+ *
+ * @return {void}
+ */
+CookieMonster.manageNextCookie = function() {
+	var barsWidth = parseInt(this.$timerBars.css('width'));
+	var timers    = [Game.goldenCookie.time, Game.goldenCookie.minTime, Game.goldenCookie.maxTime];
+	var width     = timers[2] - timers[0];
+	var countdown = Math.round(width / Game.fps);
+
+	// Update title
+	if (countdown > 0 && CookieMonster.$goldenCookie.is(':hidden')) {
+		this.goldenCookieAvailable = this.getBooleanSetting('CookieCD') ? "(" + countdown + ") " : '';
+	}
+
+	// Cancel if disabled
+	if (timers[0] <= 0 || CookieMonster.$goldenCookie.is(':visible') || !this.getBooleanSetting('CookieCD')) {
+		return this.fadeOutBar(this.colors.purple);
+	}
+
+	this.updateBar('Next Cookie', this.colors.purple, width, width / timers[2] * 100);
+	$('#cmt2_'+this.colors.purple).css('max-width', (barsWidth - 189) * 0.67 + "px");
+};
+
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////// HELPERS /////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+/**
+ * Update a buff bar's informations
+ *
+ * @param {String}  name
+ * @param {String}  color
+ * @param {Integer} timer
+ * @param {Integer} width
+ *
+ * @return {void}
+ */
+CookieMonster.updateBar = function (name, color, timer, width) {
+	var $bar = $('#cookie_monster_timer_'+color);
+	if ($bar.length !== 1) {
+		this.createBar(name, color);
+	}
+
+	// Define text count and CSS width
+	var count = timer / Game.fps;
+	if (typeof width === 'undefined') {
+		width = timer / Game.goldenCookie.maxTime * 100;
+	}
+
+	$('#cmt_'+color).css('width', width);
+	$('#cmt_time_'+color).text(Math.round(count));
+	$bar.fadeIn(250);
+};
+
+/**
+ * Append a timer bar
+ *
+ * @param {String}  color
+ * @param {Integer} count
+ *
+ * @return {void}
+ */
+CookieMonster.createBar = function (name, color) {
+	var secondBar = '';
+	if (name === 'Next Cookie') {
+		secondBar = '<div id="cmt2_'+this.colors.purple+'" style="position:relative; background:#' +this.colors.purple+'; height:10px; width:100%; margin-left:0px; max-width:0px; float:right;"></div>';
+	}
+
+	this.$timerBars.append(
+		'<div id="cookie_monster_timer_' + color + '" style="padding:4px 0px 5px 0px;">'+
+		'<table cellpadding=0 cellspacing=0 style="font-style:inherit; color:inherit; width:100%;">'+
+			'<tr>' +
+				'<td style="width:130px; text-align:right;">' + name + "<td>" +
+				'<td>'+
+					'<div id="cmt_' + color + '" style="position:relative; background:#' + color + '; height:10px; width:100%; margin-left:4px; border:1px solid black;">'+
+						secondBar +
+						'<div id="cmt_time_' + color + '" style="text-align:left; position:absolute; right:-50px; top:-5px; width:45px;">0</div>'+
+					'</div>'+
+				'</td>'+
+				'<td style="width:55px;"></td>'+
+			'</tr>' +
+		'</table>'+
+	'</div>');
+};
+
+/**
+ * Fade out a bar of a certain color
+ *
+ * @param {string} color
+ *
+ * @return {void}
+ */
+CookieMonster.fadeOutBar = function(color, match) {
+	var $bar = $("#cookie_monster_timer_" + color);
+
+	if ($bar.length === 1 && $bar.css("opacity") === "1" && color !== match) {
+		$bar.stop(true, true).fadeOut(250);
+	}
+};
