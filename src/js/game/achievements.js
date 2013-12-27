3c80eb26c27b2d52744b7257289bf7e21902e1c3
Maxime Fabre <ehtnam6@gmail.com>
2013-12-27 18:21:40 +0100

    Move building schemas methods to achievements.js

--- src/js/game/achievements.js
+++ src/js/game/achievements.js
@@ -27,3 +27,108 @@ CookieMonster.hasAchievement = function(checkedAchievement) {
 CookieMonster.hasntAchievement = function(checkedAchievement) {
 	return !this.hasAchievement(checkedAchievement);
 };
+
+//////////////////////////////////////////////////////////////////////
+//////////////////////////// BUILDING SCHEMAS ////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+CookieMonster.baseTen = function(building) {
+	if (this.hasntAchievement("Base 10")) {
+		var t = [];
+		var n = [];
+		Game.ObjectsById.forEach(function (building) {
+			t.push(building.name);
+			n.push(building.amount);
+		});
+		t.forEach(function (t, r) {
+			if (t === building) {
+				n[r]++;
+			}
+		});
+		var r = n.length * 10;
+		for (var i = 0; i < n.length; i++) {
+			if (n[i] < r) {
+				return false;
+			}
+			r = r - 10;
+		}
+		return true;
+	}
+
+	return false;
+};
+
+CookieMonster.mathematician = function(building) {
+	if (this.hasntAchievement("Mathematician")) {
+		var t = [];
+		var n = [];
+		Game.ObjectsById.forEach(function (building) {
+			t.push(building.name);
+			n.push(building.amount);
+		});
+		t.forEach(function (t, r) {
+			if (t === building) {
+				n[r]++;
+			}
+		});
+		var r = 128;
+		for (var i = 0; i < n.length; i++) {
+			if (i > 2) {
+				r = r / 2;
+			}
+			if (n[i] < r) {
+				return false;
+			}
+		}
+		return true;
+	}
+	return false;
+};
+
+/**
+ * Check if a given building would unlock OWE when bought
+ *
+ * @param {String} checkedBuilding
+ *
+ * @return {Boolean}
+ */
+CookieMonster.oneWithEverything = function(checkedBuilding) {
+	if (this.hasAchievement('One with everything')) {
+		return false;
+	}
+
+	var todo = [];
+	Game.ObjectsById.forEach(function (building) {
+		if (building.amount === 0) {
+			todo.push(building.name);
+		}
+	});
+
+	if (todo.length === 1 && todo[0] === checkedBuilding) {
+		return true;
+	}
+};
+
+/**
+ * Check if a given building will unlock Centennial when bought
+ *
+ * @param {String} checkedBuilding
+ *
+ * @return {Boolean}
+ */
+CookieMonster.centennial = function(checkedBuilding) {
+	if (this.hasAchievement('Centennial')) {
+		return false;
+	}
+
+	var todo = [];
+	Game.ObjectsById.forEach(function (building) {
+		if (building.amount < 100) {
+			todo.push(building);
+		}
+	});
+
+	if (todo.length === 1 && todo[0].name === checkedBuilding && todo[0].amount === 99) {
+		return true;
+	}
+};
