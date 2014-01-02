f8ec2e3e29c5c3fb23ddb9150179116dc9d49bf9
Maxime Fabre <ehtnam6@gmail.com>
2014-01-02 19:38:08 +0100

    Stop tracking original version

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
+# Finalize new version
+grunt rebuild
+git commit -am "Rebuild files"
 git push origin develop
+
+# Merge into master
 git checkout master
 git merge develop
 git push origin master
 
+# Deploy
 ssh digitalocean <<'ENDSSH'
 cd /home/www/cookie-monster
 git pull
