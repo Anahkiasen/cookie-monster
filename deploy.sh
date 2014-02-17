589b9e1110ce2527d75f8d296212c71a51d61b5f
Maxime Fabre <ehtnam6@gmail.com>
2014-02-17 17:46:31 +0100

    Tweak to deploy routine

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,18 @@ grunt
 git commit -am "Rebuild files"
 git push origin develop
 
+# Deploy to develop
+ssh digitalocean <<'ENDSSH'
+cd /home/www/cookie-monster-dev
+git pull
+ENDSSH
+
 # Merge into master
 git checkout master
 git merge develop
 git push origin master
 
-# Deploy
+# Deploy to master
 ssh digitalocean <<'ENDSSH'
 cd /home/www/cookie-monster
 git pull
