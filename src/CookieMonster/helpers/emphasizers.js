80e6673647d0127c75b407c805b38f94542c0f71
Aliaksei SyDr Karalenka <sydr1991@gmail.com>
2014-02-18 20:36:31 +0300

    Removed stats "wrinklers sucked cookies" and "benefits of popping" (does not give you any information) Added stat "Cookies withered" (works correctly only, if there is no wrinklers remaining from time, before Cookie Monster was loaded)

--- src/CookieMonster/helpers/emphasizers.js
+++ src/CookieMonster/helpers/emphasizers.js
@@ -1,5 +1,19 @@
 CookieMonster.Emphasizers = {};
 
+
+CookieMonster.inRect = function(x,y,rect) {
+	var dx = x+Math.sin(-rect.r)*(-(rect.h/2-rect.o)),dy=y+Math.cos(-rect.r)*(-(rect.h/2-rect.o));
+	var h1 = Math.sqrt(dx*dx + dy*dy);
+	var currA = Math.atan2(dy,dx);
+	var newA = currA - rect.r;
+	var x2 = Math.cos(newA) * h1;
+	var y2 = Math.sin(newA) * h1;
+	if (x2 > -0.5 * rect.w && x2 < 0.5 * rect.w && y2 > -0.5 * rect.h && y2 < 0.5 * rect.h) {
+		return true;
+	}
+	return false;
+};
+
 /**
  * Executes actions while something goes in and out of focus
  *
